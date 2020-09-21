e771a628e669358fff06c6862e5412f284e274a2
Irwanto <irwanto_kong@yahoo.co.id>
2020-09-21 21:38:11 +0700

    Change package name

--- app/src/androidTest/java/id/ac/ui/cs/mobileprogramming/irwanto/helloworld/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/id/ac/ui/cs/mobileprogramming/irwanto/helloworld/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package id.ac.ui.cs.mobileprogramming.helloworld;
+package id.ac.ui.cs.mobileprogramming.irwanto.helloworld;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("id.ac.ui.cs.mobileprogramming.helloworld", appContext.getPackageName());
+        assertEquals("id.ac.ui.cs.mobileprogramming.irwanto.helloworld", appContext.getPackageName());
     }
 }
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/irwanto/helloworld/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/irwanto/helloworld/MainActivity.java
@@ -1,4 +1,4 @@
-package id.ac.ui.cs.mobileprogramming.helloworld;
+package id.ac.ui.cs.mobileprogramming.irwanto.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
