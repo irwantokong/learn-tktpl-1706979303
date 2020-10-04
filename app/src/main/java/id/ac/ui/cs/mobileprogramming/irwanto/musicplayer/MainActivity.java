538fbf049a1db8141a087ef31ab39c74efea91d7
Irwanto <irwanto_kong@yahoo.co.id>
2020-10-04 17:10:34 +0700

    Override back button to show alert dialog

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/irwanto/musicplayer/MainActivity.java
@@ -0,0 +1,37 @@
+package id.ac.ui.cs.mobileprogramming.irwanto.musicplayer;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Warning!");
+        alertDialogBuilder.setMessage("Do you really want to close this app?");
+        alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+}
